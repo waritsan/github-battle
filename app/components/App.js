3e67f7eec3a1fd953f9cf9c9c66837a78c8eec42
Warit Santaputra <waritsan@gmail.com>
2017-05-21 17:53:27 +0700

    Add Results

--- app/components/App.js
+++ app/components/App.js
@@ -18,6 +18,7 @@ class App extends React.Component {
           <Switch>
             <Route exact path='/' component={Home} />
             <Route exact path='/battle' component={Battle} />
+            <Route path='/battle/results' component={Results} />
             <Route path='/popular' component={Popular} />
             <Route render={function () {
               return <p>Not Found</p>
